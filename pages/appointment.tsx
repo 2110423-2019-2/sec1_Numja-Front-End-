2ca5069361b0e229b1386b8d5d6022c8ca2e8bb6
chavincc <cc6chavin@gmail.com>
2020-02-26 13:49:38 +0700

    feat(calendar): calendar building block
    integrate and customize calendar without styling and appointment

--- pages/appointment.tsx
+++ pages/appointment.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
+
 import WithNav from '../components/layout/withNav'
 import WithSidebar from '../components/layout/withSidebar'
+import Calendar from '../components/calendar'
 
 const Appointment = () => {
   return (
     <div className="appointment">
       <WithNav>
-        <WithSidebar current={1}>appointment</WithSidebar>
+        <WithSidebar current={1}>
+          <Calendar />
+        </WithSidebar>
       </WithNav>
     </div>
   )
