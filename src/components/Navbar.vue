6496a45d309383592a9900091a35e3da3e18e3d5
UtopiaBeam <utopiabeam@gmail.com>
2020-03-09 22:45:10 +0700

    feat(navbar): add navbar
    - add navigation bar and drawer in every page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-app-bar color="primary" dark height="60px" app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Indemand</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer id="drawer" v-model="drawer" absolute temporary>
+      <v-list nav>
+        <v-list-item-group v-model="group" active-class="primary--text text--accent-4">
+          <v-list-item v-for="(menu, i) of menus" :key="i" link :to="menu.link">
+            <v-list-item-icon>
+              <v-icon>{{ menu.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Model } from "vue-property-decorator";
+
+@Component
+export default class Navbar extends Vue {
+  @Model() private drawer: boolean = false;
+
+  private menus = [
+    {
+      icon: "mdi-home",
+      title: "Home",
+      link: "/"
+    },
+    {
+      icon: "mdi-calendar-month",
+      title: "Appointment",
+      link: "/appointment"
+    },
+    {
+      icon: "mdi-chat",
+      title: "Chat",
+      link: "/chat"
+    }
+  ];
+}
+</script>
+
+<style lang="scss" scoped>
+#drawer {
+  margin-top: 60px;
+}
+</style>
