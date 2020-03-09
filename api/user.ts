1009fb4645e96262135429c35f8186d7b952c538
UtopiaBeam <utopiabeam@gmail.com>
2020-03-09 21:27:39 +0700

    init: migrate project to Vue

--- api/user.ts
+++
@@ -1,8 +0,0 @@
-import { DOMAIN_NAME } from '../utils/constants'
-import axios from 'axios'
-
-const userLogin = async (username, password) => {
-  const response = await axios.get(`${DOMAIN_NAME}/auth/login`)
-}
-
-export { userLogin }
