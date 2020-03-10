4531eb9846565ed609355c8e166bb7a3f8bedde8
UtopiaBeam <utopiabeam@gmail.com>
2020-03-10 14:20:32 +0700

    feat(axios): add axios

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios);
+Vue.axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 
 new Vue({
   router,
