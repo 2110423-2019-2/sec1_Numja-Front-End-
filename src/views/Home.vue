ade9949be8c4593b0b2f951f8850e6199301d1c9
UtopiaBeam <utopiabeam@gmail.com>
2020-03-10 21:35:18 +0700

    feat(home): update temporary home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,9 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component
+export default class Home extends Vue {}
 </script>
