893860c53f993ea49bbeca64e68b217aec93b945
UtopiaBeam <utopiabeam@gmail.com>
2020-03-11 22:34:25 +0700

    feat(firebase): connect to firebase

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,27 @@ import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
 Vue.axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
