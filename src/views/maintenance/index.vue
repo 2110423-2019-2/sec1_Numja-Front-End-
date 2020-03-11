bd75da1a7be062819fe8f1225bd37153d1fa3879
Tanawit Kritwongwimian <tanawit139@gmail.com>
2020-03-11 13:01:49 +0700

    feat(maintenance): add router for maintenance feature

---
+++ src/views/maintenance/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+export default class Maintenance extends Vue {}
+</script>
+
+<style scoped></style>
