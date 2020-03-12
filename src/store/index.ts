7c40918a91af9be181e134fac59ac1e06db55524
UtopiaBeam <utopiabeam@gmail.com>
2020-03-12 08:03:09 +0700

    feat(chat): add chat vuex module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import VuexPersistence from "vuex-persist";
+import chat from "@/store/modules/chat";
 import login from "@/store/modules/login";
 import snackbar from "@/store/modules/snackbar";
 import suspendUser from "@/store/modules/maintenance/suspend";
@@ -15,6 +16,7 @@ const vuexLocal = new VuexPersistence<LoginState>({
 
 export default new Vuex.Store<any>({
   modules: {
+    chat,
     login,
     snackbar,
     suspendUser
